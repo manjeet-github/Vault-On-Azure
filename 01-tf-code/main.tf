6c42dd466fd9ecd201f261e730dea92c9ffd981a
Manjeet <manjeet@hashicorp.com>
2020-01-06 16:41:54 -0500

    test 3

--- 01-tf-code/main.tf
+++ 01-tf-code/main.tf
@@ -4,6 +4,6 @@ provider "azurerm" {
 // Create the resource groups
 resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-resources"
-  location = "${var.location}"
-  tags     = "${var.tags}"
+  location = var.location
+  tags     = var.tags
 }
