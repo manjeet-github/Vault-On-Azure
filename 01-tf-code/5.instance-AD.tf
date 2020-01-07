673728f0df6afabefdf76bf9b19641b53b0834da
Manjeet <manjeet@hashicorp.com>
2020-01-07 08:53:45 -0500

    added AD package install

--- 01-tf-code/5.instance-AD.tf
+++ 01-tf-code/5.instance-AD.tf
@@ -5,6 +5,16 @@ locals {
   custom_data_params   = "Param($RemoteHostName = \"${local.virtual_machine_fqdn}\", $ComputerName = \"${local.virtual_machine_name_AD}\")"
   custom_data_content  = "${local.custom_data_params} ${file("./files/winrm.ps1")}"
 
+  // The below locals to build the command to install all the windows packages for AD
+  // the `exit_code_hack` is to keep the VM Extension resource happy
+  import_command       = "Import-Module ADDSDeployment"
+  password_command     = "$password = ConvertTo-SecureString ${data.azurerm_key_vault_secret.myWinPass.value} -AsPlainText -Force"
+  install_ad_command   = "Add-WindowsFeature -name ad-domain-services -IncludeManagementTools"
+  configure_ad_command = "Install-ADDSForest -CreateDnsDelegation:$false -DomainMode Win2012R2 -DomainName ${var.active_directory_domain} -DomainNetbiosName ${var.active_directory_netbios_name} -ForestMode Win2012R2 -InstallDns:$true -SafeModeAdministratorPassword $password -Force:$true"
+  shutdown_command     = "shutdown -r -t 10"
+  exit_code_hack       = "exit 0"
+  powershell_command   = "${local.import_command}; ${local.password_command}; ${local.install_ad_command}; ${local.configure_ad_command}; ${local.shutdown_command}; ${local.exit_code_hack}"
+
 }
 
 # - create a public ip to be attached to the VM NIC
@@ -181,4 +191,22 @@ resource "azurerm_virtual_machine" "windows-vm" {
     ]
   }
 
+}
+
+// NOTE: we **highly recommend** not using this configuration for your Production Environment
+// this provisions a single node configuration with no redundancy.
+resource "azurerm_virtual_machine_extension" "create-active-directory-forest" {
+  name                 = "create-active-directory-forest"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = var.location
+  virtual_machine_name = azurerm_virtual_machine.windows-vm.name
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
+
+  settings = <<SETTINGS
+    {
+        "commandToExecute": "powershell.exe -Command \"${local.powershell_command}\""
+    }
+SETTINGS
 }
