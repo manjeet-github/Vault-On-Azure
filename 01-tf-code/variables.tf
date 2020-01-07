4ee14e270a3ada7858c8b8bc78ab4e5ac3d3ff54
Manjeet <manjeet@hashicorp.com>
2020-01-06 19:31:46 -0500

    fixes

--- 01-tf-code/variables.tf
+++ 01-tf-code/variables.tf
@@ -46,3 +46,58 @@ variable "subnet_prefix" {
   default     = "10.0.10.0/24"
 }
 
+# - Variables below for the virtual machines - Win AD Server, Win Client
+# - The password associated with the local administrator account on the virtual machine
+# - This is stored in Vault and is fetched from the Vault
+
+variable "vault_id_for_password" {
+  description = "secret name in vault which stores the password for windows vm"
+}
+
+# - The username associated with the local administrator account on the virtual machine
+# - This is stored in Vault and is fetched from the Vault
+variable "vault_id_for_username" {
+  description = "secret name in vault which stores the username for windows vm"
+}
+
+variable "vm_name" {
+  description = "Name of the virtual machine"
+  default     = "windows-vm"
+}
+
+variable "vmcount" {
+  description = "number of virtual machines"
+  default     = "1"
+}
+
+variable "vmsize" {
+  description = "VM Size for the Production Environment"
+  type        = map(string)
+
+  default = {
+    small      = "Standard_DS1_v2"
+    medium     = "Standard_D2s_v3"
+    large      = "Standard_D4s_v3"
+    extralarge = "Standard_D8s_v3"
+  }
+}
+
+variable "os_ms" {
+  description = "Operating System Image - MS Windows"
+  type        = map(string)
+
+  default = {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
+
+variable "active_directory_domain" {
+  description = "The name of the Active Directory domain, for example `hashisingh.local`"
+}
+
+variable "active_directory_netbios_name" {
+  description = "The netbios name of the Active Directory domain, for example `hashisingh`"
+}
